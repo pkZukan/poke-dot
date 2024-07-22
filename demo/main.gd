c4ee043d378d751c703fc2228c81493650b043aa
Rei <asterma@yahoo.com>
2024-07-22 10:06:59 -0400

    Add AO and fix UV in shader

--- demo/main.gd
+++ demo/main.gd
@@ -2,11 +2,16 @@
 extends Node3D
 
 func _ready():
-	var mdl:TrinityModel = TrinityModel.new()
-	#mdl.Load("res://pm0004_00_00/", "pm0004_00_00.trmdl")
-	mdl.Load("res://tr0380_musician/", "tr0380_00.trmdl")
-	mdl.position = Vector3.ZERO
-	add_child(mdl)
+	var trainer:TrinityModel = TrinityModel.new()
+	trainer.Load("res://tr0380_musician/", "tr0380_00.trmdl")
+	trainer.position = Vector3.ZERO
+	
+	var pkmn = TrinityModel.new()
+	pkmn.Load("res://pm0004_00_00/", "pm0004_00_00.trmdl")
+	pkmn.position = Vector3(1.0,0.0,0.0)
+	
+	add_child(trainer)
+	add_child(pkmn)
 
 func _process(delta):
 	pass
