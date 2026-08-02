b61909e570c7fddfa0fc3e4143aee528bce898b8
Rei <asterma@yahoo.com>
2026-08-02 18:53:15 -0400

    Add more city mesh; add more shaders

--- demo/main.gd
+++ demo/main.gd
@@ -9,3 +9,10 @@ func _ready():
 	$Mountains/Enkei.load_model("res://Assets/field/model/env/enkei/enkei1", "enkei_mountains1.trmdl")
 	$Mountains/Enkei2.load_model("res://Assets/field/model/env/enkei/enkei1", "enkei_mountains2.trmdl")
 	$Mountains/Range.load_model("res://Assets/field/model/env/enkei/enkei1", "enkei_mountain_range.trmdl")
+	
+	var city = TrinityModel.new()
+	city.load_model("res://Assets/field/model/t1/t1_ev/t1_ev001/", "t1_ev001_city.trmdl")
+	$City.add_child(city)
+	var water = TrinityModel.new()
+	water.load_model("res://Assets/field/model/t1/t1_ev/t1_ev001/", "t1_ev001_water.trmdl")
+	$City.add_child(water)
