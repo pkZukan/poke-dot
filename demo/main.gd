7dc9720eef5229899433161fb42411ea2793b90a
Rei <asterma@yahoo.com>
2026-08-06 13:56:19 -0400

    Load proper overworld models; fix some shaders; misc

--- demo/main.gd
+++ demo/main.gd
@@ -1,8 +1,35 @@
 @tool
 extends Node
 
+func get_trmdl_files_recursive(path: String) -> Array[String]:
+	var results: Array[String] = []
+	var dir := DirAccess.open(path)
+
+	if dir == null:
+		push_error("Failed to access path: " + path)
+		return results
+
+	dir.list_dir_begin()
+	var file_name := dir.get_next()
+
+	while file_name != "":
+		# Skip hidden self/parent directory references
+		if file_name != "." and file_name != "..":
+			var full_path := path.path_join(file_name)
+
+			if dir.current_is_dir():
+				# Recursively search subdirectories
+				results.append_array(get_trmdl_files_recursive(full_path))
+			elif file_name.get_extension() == "trmdl":
+				results.append(full_path)
+
+		file_name = dir.get_next()
+
+	dir.list_dir_end()
+	return results
+
 func _ready():		
-	#Load static objects
+	#Outter mountains
 	var land_enkei = TrinityModel.new()
 	land_enkei.load_model("res://Assets/field/model/env/enkei/enkei1", "enkei_landscape1.trmdl")
 	$Landscape.add_child(land_enkei)
@@ -20,13 +47,32 @@ func _ready():
 	mtn_range.load_model("res://Assets/field/model/env/enkei/enkei1", "enkei_mountain_range.trmdl")
 	$Mountains.add_child(mtn_range)
 	
-	var city = TrinityModel.new()
-	city.load_model("res://Assets/field/model/t1/t1_ev/t1_ev001/", "t1_ev001_city.trmdl")
-	$City.add_child(city)
-	var water = TrinityModel.new()
-	water.load_model("res://Assets/field/model/t1/t1_ev/t1_ev001/", "t1_ev001_water.trmdl")
-	$City.add_child(water)
+	#Buildings
+	for i in range(1, 25):
+		var path: String = "res://Assets/field/model/t1/t1_b/t1_b%02d_1/t1_b%02d_1.trmdl" % [i,i];
+		var dir_path: String = path.get_base_dir() 
+		var file_name: String = path.get_file()
+		if not FileAccess.file_exists(path):
+			continue
+		var building = TrinityModel.new()
+		building.load_model(dir_path, file_name)
+		building.name = file_name.get_basename()
+		$City/Buildings.add_child(building)
+		building.owner = get_tree().edited_scene_root
+		
+	#Ground
+	for path in get_trmdl_files_recursive("res://Assets/field/model/t1/t1_g"):
+		var dir_path: String = path.get_base_dir() 
+		var file_name: String = path.get_file()
+		if file_name == "t1_g13_5_replace01":
+			continue
+		var ground = TrinityModel.new()
+		ground.load_model(dir_path, file_name)
+		ground.name = file_name.get_basename()
+		$City/Ground.add_child(ground)
+		ground.owner = get_tree().edited_scene_root
 	
+	#Tower
 	var prism = TrinityModel.new()
 	prism.load_model("res://Assets/field/model/t1/t1_b/t1_b_prism/", "t1_b_prism_bottom.trmdl")
 	$Tower.add_child(prism)
